748862b51e4c6b9b7349e965825c62e0d4e66ea3
dreacter <dreacter@outlook.com>
2018-04-21 13:21:30 +0800

    Init

---
+++ arpspoofing.h
@@ -0,0 +1,16 @@
+#ifndef ARPSPOOFING_H
+#define ARPSPOOFING_H
+
+#include <QObject>
+#include <QThread>
+
+class ARPSpoofing : public QThread
+{
+    Q_OBJECT
+
+signals:
+
+public slots:
+};
+
+#endif // ARPSPOOFING_H
